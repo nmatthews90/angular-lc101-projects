8b37b92ab0fe4f5eb3cc1b872defe634357dd876
Natasha Matthews <natashamatthews90@gmail.com>
2021-12-08 17:59:29 -0600

    added inputs to the movie list

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-   selector: 'movie-list',
-   templateUrl: './movie-list.component.html',
-   styleUrls: ['./movie-list.component.css']
+  selector: "movie-list",
+  templateUrl: "./movie-list.component.html",
+  styleUrls: ["./movie-list.component.css"],
 })
 export class MovieListComponent implements OnInit {
-   movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
+  movies = ["Toy Story", "The Shining", "Sleepless in Seattle", "The Martian"];
 
-   constructor() { }
+  constructor() {}
 
-   ngOnInit() {
-   }
+  ngOnInit() {}
+
+  addMovie(newTitle: string) {
+    let errorMsg = "";
+    if (newTitle === "") {
+      errorMsg = "Please enter a movie title.";
+    } else if (this.movies.includes(newTitle)) {
+      errorMsg = `${newTitle} is already in your movie list.`;
+    } else {
+      this.movies.push(newTitle);
+    }
+    return errorMsg;
+  }
 }
