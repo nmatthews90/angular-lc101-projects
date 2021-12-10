1b347387494ea863b7151c405f9da42a6b5798a3
Natasha Matthews <natashamatthews90@gmail.com>
2021-12-10 09:53:39 -0600

    updated the skill-set and the buttons

--- lesson3/examples/src/app/skill-set/skill-set.component.ts
+++ lesson3/examples/src/app/skill-set/skill-set.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-   selector: 'skill-set',
-   templateUrl: './skill-set.component.html',
-   styleUrls: ['./skill-set.component.css']
+  selector: "skill-set",
+  templateUrl: "./skill-set.component.html",
+  styleUrls: ["./skill-set.component.css"],
 })
 export class SkillSetComponent implements OnInit {
-   listHeading: string = 'Some Coding Skills I Know';
-   skills: string[] = ['Loops', 'Conditionals', 'Functions', 'Classes', 'Modules', 'Git', 'HTML/CSS'];
-   alternateColor: string = 'black';
-   bulletType: string = 'A';
-   changeColor: boolean = true;
+  listHeading: string = "Some Coding Skills I Know";
+  skills: string[] = [
+    "Loops",
+    "Conditionals",
+    "Functions",
+    "Classes",
+    "Modules",
+    "Git",
+    "HTML/CSS",
+  ];
+  alternateColor: string = "black";
+  bulletType: string = "A";
+  changeColor: boolean = true;
 
-   constructor() { }
+  constructor() {}
 
-   ngOnInit() { }
-
+  ngOnInit() {}
 }
